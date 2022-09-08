559d1146e9f92910e543e870e973109b12fae9ae
N3sh47 <munenejohn628@gmail.com>
2022-09-08 08:23:29 +0300

    initial commit

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220905.211503';
+const cacheName = 'chirpy-20220907.204758';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
