1a747228c14901625b3919e582c8f0984fc8e7bf
N3sh47 <munenejohn628@gmail.com>
2022-10-04 19:30:51 +0300

    updated blog

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221004.162306';
+const cacheName = 'chirpy-20221004.163019';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
